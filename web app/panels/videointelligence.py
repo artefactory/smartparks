00783f30be00a790b7472b5b45b9a746d8efea81
SimoneGayed <102957647+SimoneGayed@users.noreply.github.com>
2023-05-09 16:19:54 +0200

    Update videointelligence.py

--- web app/panels/videointelligence.py
+++ web app/panels/videointelligence.py
@@ -53,12 +53,12 @@ def app():
     if len(selected_date) == 2:
 
         df = run_query(
-            f"SELECT * FROM `{PROJECT}.videos.{selected_camera_trap}`"
+            f"SELECT * FROM `{PROJECT}.videos.{selected_camera_trap}` ORDER BY timestamp DESC"
         )
 
         df = filter_dataframe_by_date_time_range(df, selected_date, selected_time)
 
-        for index, row in df.iterrows():
+        for _, row in df.iterrows():
 
             video = row["uri"].replace(f"gs://{BUCKET_NAME}/", "")
 
@@ -89,47 +89,3 @@ def app():
             # display annotations
             if number_of_people != None:
                 col2.text(number_of_people)
-
-            # if selected_use_case == "label detection":
-            #     # display video
-            #     vid_col.video(source_vid)
-
-            #     # get labels
-            #     labels = get_video_labels(
-            #         json.loads(row["response"]), selected_use_case
-            #     )
-
-            #     # display labels
-            #     for label in labels:
-            #         col2.text(label + ": " + str(round(labels[label] * 100, 2)) + "%")
-            #         col2.progress(labels[label])
-
-            # elif selected_use_case == "object detection":
-
-            #     annotated_vid = read_media(OUTPUT_BUCKET_NAME, video)
-            #     vid_col.video(annotated_vid)
-
-            #     # annotate_video(json.loads(row["response"]), BUCKET_NAME, video)
-
-            #     # # display video
-            #     # vid_col.video(source_vid)
-
-            #     # get labels
-            #     labels = get_video_labels(
-            #         json.loads(row["response"]), selected_use_case
-            #     )
-
-            #     # display labels
-            #     for label in labels:
-            #         col2.text(label + ": " + str(round(labels[label] * 100, 2)) + "%")
-            #         col2.progress(labels[label])
-
-            # elif selected_use_case == "people detection":
-            #     # display video
-            #     vid_col.video(source_vid)
-
-            #     # get annotations
-            #     number_of_people = get_number_of_people(json.loads(row["response"]))
-
-            #     # display annotations
-            #     col2.text(number_of_people)
